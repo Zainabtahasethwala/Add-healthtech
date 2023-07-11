acd12af7a8696d4c342b9252195873ac8ad5925e
zainabsethwala <zainab.s@applocum.org>
2023-07-11 14:55:23 +0530

    dropdown image & custom accordion

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,18 @@ $(document).ready(function(){
             $('header').removeClass('sticky');
           }
     });
-});
+    //accordion js
+    $('.accordion_parent .accordion-item:nth-child(1) .accordion-tittle').addClass('active');
+    $('.accordion_parent .accordion-item:nth-child(1) .accordion-content').slideDown();
+    $('.accordion-tittle').click(function(){
+      if ($(this).hasClass('active')) {
+        $(this).siblings('.accordion-content').slideUp();
+        $(this).removeClass('active');
+      } else {
+        $('.accordion-content').slideUp();
+        $('.accordion-tittle').removeClass('active');
+        $(this).siblings('.accordion-content').slideToggle();
+        $(this).toggleClass('active');
+      }
+    });
+  });
