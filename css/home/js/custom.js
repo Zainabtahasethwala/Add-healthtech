1a9f5c25a71a834a648eda216dd4bb2700072b2e
zainabsethwala <zainab.s@applocum.org>
2023-07-17 19:31:23 +0530

    change

---
+++ css/home/js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.menutoggle').click(function(){
+        $('body').toggleClass('opened');
+        $('.menu_item').toggleClass('menuopened');
+    });
+    $(window).scroll(function(){
+        if ($(this).scrollTop()>1) {
+            $('header').addClass('sticky');
+          } else {
+            $('header').removeClass('sticky');
+          }
+    });
+    //accordion js
+    // $('.accordion_parent .accordion-item:nth-child(1) .accordion-tittle').addClass('active');
+    // $('.accordion_parent .accordion-item:nth-child(1) .accordion-content').slideDown();
+    $('.accordion-tittle').click(function(){
+      if ($(this).hasClass('active')) {
+        $(this).siblings('.accordion-content').slideUp();
+        $(this).removeClass('active');
+      } else {
+        $('.accordion-content').slideUp();
+        $('.accordion-tittle').removeClass('active');
+        $(this).siblings('.accordion-content').slideToggle();
+        $(this).toggleClass('active');
+      }
+    });
+  });
